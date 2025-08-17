7bf6654c3997696d19d94a41d995dd7415647f46
Charran Thangeswaran <126629370+cthanges@users.noreply.github.com>
2025-08-17 19:19:02 -0400

    Add files via upload

---
+++ VGA-Video-Game-Processor/_impact.cmd
@@ -0,0 +1,21 @@
+setMode -bs
+setMode -bs
+setMode -bs
+setCable -port auto
+Identify -inferir 
+identifyMPM 
+assignFile -p 1 -file "/home/student2/s4jabbar/COE758/Project2/PONG.bit"
+Program -p 1 
+setMode -bs
+setMode -bs
+deleteDevice -position 1
+deleteDevice -position 1
+deleteDevice -position 1
+setMode -bs
+setMode -ss
+setMode -sm
+setMode -hw140
+setMode -spi
+setMode -acecf
+setMode -acempm
+setMode -pff
